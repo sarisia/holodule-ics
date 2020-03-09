4158ccfe75f60c7bae363fcb3279fe7068d6893c
Sarisia <sarisiaice@gmail.com>
2020-03-09 20:27:15 +0900

    main: fix User-Agent

--- holodule/holodule.py
+++ holodule/holodule.py
@@ -25,7 +25,7 @@ class Holodule():
     async def run(self) -> None:
         self.session = ClientSession(
             timeout=ClientTimeout(total=30),
-            headers={ "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36" }
+            headers={ "User-Agent": "Holodule-ICS" }
         )
 
         index_html = await self.get_page()
